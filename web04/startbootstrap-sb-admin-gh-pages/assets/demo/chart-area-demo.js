da289121967d772f3a5ebb7b36df6c8ee35d64ee
Kjo <krt8599@naver.com>
2024-12-12 14:06:53 +0900

    자바스크립트 입문

--- web04/startbootstrap-sb-admin-gh-pages/assets/demo/chart-area-demo.js
+++ web04/startbootstrap-sb-admin-gh-pages/assets/demo/chart-area-demo.js
@@ -7,17 +7,17 @@ var ctx = document.getElementById("myAreaChart");
 var myLineChart = new Chart(ctx, {
   type: 'line',
   data: {
-    labels: ["Mar 1", "Mar 2", "Mar 3", "Mar 4", "Mar 5", "Mar 6", "Mar 7", "Mar 8", "Mar 9", "Mar 10", "Mar 11", "Mar 12", "Mar 13"],
+    labels: ["진오 1", "Mar 2", "Mar 3", "Mar 4", "Mar 5", "Mar 6", "Mar 7", "Mar 8", "Mar 9", "Mar 10", "Mar 11", "Mar 12", "Mar 13"],
     datasets: [{
       label: "Sessions",
       lineTension: 0.3,
       backgroundColor: "rgba(2,117,216,0.2)",
-      borderColor: "rgba(2,117,216,1)",
+      borderColor: "red",
       pointRadius: 5,
       pointBackgroundColor: "rgba(2,117,216,1)",
       pointBorderColor: "rgba(255,255,255,0.8)",
       pointHoverRadius: 5,
-      pointHoverBackgroundColor: "rgba(2,117,216,1)",
+      pointHoverBackgroundColor: "rgba(20,200,216,1)",
       pointHitRadius: 50,
       pointBorderWidth: 2,
       data: [10000, 30162, 26263, 18394, 18287, 28682, 31274, 33259, 25849, 24159, 32651, 31984, 38451],
