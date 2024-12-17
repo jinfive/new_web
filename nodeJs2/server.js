b30973bd830547dc2bcb12cd958eda42ec387270
Kjo <krt8599@naver.com>
2024-12-17 17:46:27 +0900

    server 수정

--- nodeJs2/server.js
+++ nodeJs2/server.js
@@ -1,25 +1,20 @@
 const express = require("express");
 
-const app = express(); //express할 수 있는 객체 리턴
+const app = express();
 
 app.get("/", (req, res) => {
-  // http header
   console.log("주소로 GET요청 들어옴");
-
-  res.status(200);
-  res.send("Hello, Express!"); //http body
+  res.status(200).send("Hello, Express!");
 });
 
 app.get("/contacts/:id", (req, res) => {
   console.log("/contacts/:id GET요청 들어옴");
-  let id = req.params.id; //params object
-  res.status(200);
-  res.send("Your id>> " + id); //ㅏ
-  // res.send("Your id>> " + req.query.id); //query object
-  // res.send("Your id>> " + req.body.id); //body object
+  const id = req.params.id;
+  const message = `Your id>> ${id}`;
+  console.log(message);
+  res.status(200).send(message);
 });
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
-  console.log("S");
 });
